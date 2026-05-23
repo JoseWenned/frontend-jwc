bff8d56ab8167a803587d71f276e608f23790391
José Wenned <wennedchaves1997@gmail.com>
2026-05-23 17:12:34 -0300

    feat: Implements component header with navigation and button

--- src/presentation/components/header/header.component.tsx
+++ src/presentation/components/header/header.component.tsx
@@ -1,7 +1,11 @@
+import { Navigation } from "../navigation/navigation.component"
+
 export const Header = () => {
     return (
         <>
             <h1>JWC Distribuição Logística</h1>
+            <Navigation/>
+            <button><img src="path/to/icon.png" alt="Fale conosco"/>Fale conosco</button>
         </>
     )
 }
