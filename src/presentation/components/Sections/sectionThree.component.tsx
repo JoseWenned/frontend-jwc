fe60efdb727b0c5dca49561f4811fa4cd867fd9e
José Wenned <wennedchaves1997@gmail.com>
2026-07-25 10:01:21 -0300

    feat: create section three

---
+++ src/presentation/components/Sections/sectionThree.component.tsx
@@ -0,0 +1,49 @@
+import "./sectionThree.style.scss";
+//Imagens icons
+import iconMissao from '../../../assets/images/icons/icons8-troféu-30.svg';
+import iconVisao from '../../../assets/images/icons/icons8-olho-30.svg';
+import iconValores from '../../../assets/images/icons/icons8-estrela-30.svg';
+import iconMapa from '../../../assets/images/icons/ChatGPT_Image_24_de_jul._de_2026__23_58_59-removebg-preview.png';
+
+import { useSlogans } from "../../hooks/slogans.hook";
+
+export const SectionThree = () => {
+    const { sectionRef, isVisible } = useSlogans();
+    
+    return (
+        <section 
+            ref={sectionRef}
+            className="containerSectionThree"
+        >
+            <div className="containerSectionThreeContent">
+                <div className="containerSectionThreeContentArticles">
+                    <article className={`articleSectionThree ${isVisible ? "show" : ""}`}>
+                        <div className="containerSectionThreeContentArticlesTitle">
+                            <img src={iconMissao} alt="icone missão" />
+                            <h2 className="titleSectionThree">MISSÃO</h2>   
+                        </div>
+                        <p className="textSectionThree">Distribuir produtos de qualidade com eficiência logística, fortalecendo parcerias comerciais e contribuindo para o desenvolvimento econômico.</p>
+                    </article>
+                    <article className={`articleSectionThree ${isVisible ? "show" : ""}`}>
+                        <div className="containerSectionThreeContentArticlesTitle">
+                            <img src={iconVisao} alt="icone visão" />
+                            <h2 className="titleSectionThree">VISÃO</h2>
+                        </div>
+                        <p className="textSectionThree">Ser reconhecida como uma das principais distribuidoras da região Nordeste, destacando-se pela excelência operacional, confiança e compromisso com clientes, fornecedores e parceiros.</p>
+                    </article>
+                    <article className={`articleSectionThree ${isVisible ? "show" : ""}`}>
+                        <div className="containerSectionThreeContentArticlesTitle">
+                            <img src={iconValores} alt="icone valores" />
+                            <h2 className="titleSectionThree">VALORES</h2>
+                        </div>
+                        <p className="textSectionThree">Trabalho, comprometimento, qualidade, sustentabilidade com ética e transparência.</p>
+                    </article>
+                </div>
+
+                <div className={`containerSectionThreeContentMap ${isVisible ? "show" : ""}`}>
+                    <img className="imgSectionThree" src={iconMapa} alt="icone mapa" />
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/presentation/pages/home/home.page.tsx
+++ src/presentation/pages/home/home.page.tsx
@@ -17,6 +17,7 @@ import { Header } from '../../components/header/header.component';
 
 //Hooks
 import { useSlogans } from '../../hooks/slogans.hook';
+import { SectionThree } from "../../components/Sections/sectionThree.component";
 
 
 export const HomePage = () => {
@@ -102,38 +103,7 @@ export const HomePage = () => {
                     </button>                
                 </div>
             </section>
-            {/* <section>
-                <h2>Buscamos distribuidores parceiros em todo o Nordeste</h2>
-                <button><img src="path/to/icon.png"/>Fale conosco</button>
-                <button>Receber tabela de preços</button>
-            </section>
-            <section>
-                <p>Vantagens de ser nosso parceiro</p>
-                <h2>Mais benefícios para o seu negócio crescer</h2>
-
-                <ul>
-                    <li>
-                        <img src="path/to/icon.png"/>
-                        <h4>Alta saída</h4>
-                        <p>Produtos com grande aceitação no mercado e giro garantido.</p>
-                    </li>
-                    <li>
-                        <img src="path/to/icon.png"/>
-                        <h4>Margem Atrativa</h4>
-                        <p>30% a 40% de margem para nossos parceiros.</p>
-                    </li>
-                    <li>
-                        <img src="path/to/icon.png"/>
-                        <h4>Entrega confiável</h4>
-                        <p>Logística rápida e segura com frota terceirizada especializada.</p>
-                    </li>
-                    <li>
-                        <img src="path/to/icon.png"/>
-                        <h4>Suporte comercial</h4>
-                        <p>Apoio completo da nossa equipe para impulsionar suas vendas.</p>
-                    </li>
-                </ul>
-            </section> */}
+            <SectionThree/>
         </>
     )
 }
